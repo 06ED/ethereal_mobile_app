1e3d588ca365c6e80b29cc6cfc69644866dee58c
06ED <hsbest14@mail.ru>
2023-03-24 11:22:19 +0300

    Added material app widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,20 @@ class Main extends StatefulWidget {
 class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            color: Color.fromARGB(255, 100, 0, 0)),
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children:  const [
-            InputFormField("Text"),
-            InputFormField("Text2"),
-            ValidateFormButton("Validate")
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Color.fromARGB(255, 100, 0, 0)),
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children:  const [
+              InputFormField("Text"),
+              InputFormField("Text2"),
+              ValidateFormButton("Validate")
+            ],
+          ),
         ),
       ),
     );
